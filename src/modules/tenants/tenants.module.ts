133d7ff7fed6e03cd977d0d20b8e48973bd41008
Victor Preston <prestonvictor25@gmail.com>
2026-05-10 01:27:19 +0300

    feat(onboarding): wire OnboardingService into TenantService and TenantsModule, add tests

--- src/modules/tenants/tenants.module.ts
+++ src/modules/tenants/tenants.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Tenant } from '../../database/entities/tenant.entity';
+import { Role } from '../../database/entities/role.entity';
+import { Permission } from '../../database/entities/permission.entity';
 import { TenantService } from './services';
+import { OnboardingService } from './services/onboarding.service';
 import { TenantController } from './tenants.controller';
 import { KafkaModule } from '../kafka/kafka.module';
 import { RedisModule } from '../redis';
@@ -9,12 +12,12 @@ import { AuditModule } from '../audit/audit.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Tenant]),
+    TypeOrmModule.forFeature([Tenant, Role, Permission]),
     KafkaModule,
     RedisModule,
     AuditModule,
   ],
-  providers: [TenantService],
+  providers: [TenantService, OnboardingService],
   controllers: [TenantController],
   exports: [TenantService],
 })
