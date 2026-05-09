5bdad00802839d9579033249533ec35f4f29c0d7
Victor Preston <prestonvictor25@gmail.com>
2026-05-10 01:19:55 +0300

    feat(password-reset): add forgot and reset password endpoints to AuthModule

--- src/modules/auth/services/index.ts
+++ src/modules/auth/services/index.ts
@@ -3,3 +3,4 @@ export * from './login.service';
 export * from './refresh-token.service';
 export * from './token-generator.service';
 export * from './change-password.service';
+export * from './password-reset.service';
